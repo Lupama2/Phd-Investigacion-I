6e610a6f0cafb3ca4da0ca2e0e672798d3f71469
Pablo Chehade <pablo.chehade.villalba@gmail.com>
2025-05-27 15:58:01 -0300

    [ISSUE #004] Develop code for instability identification in fun_identification.py

--- src/lib/fun_identification.py
+++ src/lib/fun_identification.py
@@ -11,3 +11,24 @@ def maximum_temperature(sol, verbose = False):
         print("Maximum temperature value:", max_temp_value)
 
     return max_temp_index, max_temp_value
+
+def instability(sol, verbose = False):
+    # Identifico si la inestabilidad/radio llegó a la unidad
+
+    instability_index = np.argmax(np.abs(sol.y[3] / sol.y[0]))
+    instability_value = sol.y[3][instability_index] / sol.y[0][instability_index]
+    
+    if verbose:
+        print(f"Instability index: {instability_index}\nValue: {instability_value}")
+
+    if instability_value >= 1:
+        instability_bool = True
+        if verbose:
+            print("Instability reached or exceeded the unit.")
+    else:
+        instability_bool = False
+        if verbose:
+            print("Instability did not reach the unit.")
+
+    return instability_index, instability_value, instability_bool
+        
