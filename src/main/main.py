a57c88d07ca447f1cbaee8776ffbec81be955aa7
Pablo Chehade <pablo.chehade.villalba@gmail.com>
2025-05-27 15:59:38 -0300

    [ISSUE #005] Develop function to optimize fun_optimize.py

--- src/main/main.py
+++ src/main/main.py
@@ -3,67 +3,45 @@
 ##########################
 import numpy as np
 
-#Import functions from fun_differential_equations
-from src.lib import fun_differential_equations #Esta
-from src.lib.fun_differential_equations import fun
+from src.lib.fun_optimize import optimize_pressure
 
-#Import parameters
-from src.main.parameters import T_AMB, PATM
 
-#Initial conditions
-R0 = 1e-3
-RDot0 = 0.
-T0 = T_AMB
-a0 = 1e-5
-aDot0 = 0.
 
 #Pressure parameters
-A_ps = np.array([1.])
-f_ps = np.array([1e3])
-d_ps = np.array([0.])
-
+A_ps = 1.
+f_ps = 1e3
 # Amp_limits = [0,PATM]
 # freq_limits:tuple[float] = (1e3,2e6)
 
+# Call the optimization function
+optimize_pressure(A_ps, f_ps)
 
-#Simulation parameters
-t_fin = 1e-3
+#Uso Bayesian Optimization de Optuna para maximizar la temperatura máxima
+import optuna
+from src.main.parameters import PATM
 
+def objective(trial):
+    # Define the search space for the parameters, ambos log
+    A_ps = trial.suggest_float('A_ps', 1, PATM, log=True)  # Pressure amplitude
+    f_ps = trial.suggest_float('f_ps', 1e3, 2e6, log=True)  # Pressure frequency
 
+    # Call the optimization function
+    max_temp_value, instability_bool = optimize_pressure(A_ps, f_ps)
 
-#arguments of fun
-ps_argument = [A_ps, f_ps, d_ps]
-y0 = [R0, RDot0, T0, a0, aDot0]  # Initial conditions
+    # Return the maximum temperature value as the objective to maximize
+    return max_temp_value
 
-#Imprimo tipos de datos
-for i, arg in enumerate(ps_argument):
-    print(f"Argument {i}: {type(arg)}")
-for i, arg in enumerate(y0):
-    print(f"y0[{i}]: {type(arg)}")
+# Create a study object
+study = optuna.create_study(direction="maximize")
 
-fun(0.,y0,ps_argument)
+# Optimize the objective function
+study.optimize(objective, n_trials=10000, n_jobs=-1)
 
-#Resuelvo fun con solve_ivp
-from scipy.integrate import solve_ivp
+# Save study
+import joblib
+joblib.dump(study, 'data/outputs/study.pkl')
 
-sol = solve_ivp(fun, [0, t_fin], y0, args=(ps_argument,), method='RK45', rtol
-=1e-6, atol=1e-9)
-# Print the solution
-print("Time points:", sol.t)
+# Ejecutar nuevamente el mejor caso
+best_params = study.best_params
 
-#Plot
-from src.utility.graphics import plot_results
-
-#Creo arrays A_ps_array que tiene el mismo tamaño que sol.t. Recordemos que A_ps ya es un numpy array
-# A_ps_array = np.tile(A_ps, (len(sol.t), 1)).T[0].tolist()
-# f_ps_array = np.tile(f_ps, (len(sol.t), 1)).T[0].tolist()
-# d_ps_array = np.tile(d_ps, (len(sol.t), 1)).T[0].tolist()
-
-# print("A_ps_array:", A_ps_array)
-
-# #Imprimo tipos de datos
-# print("Type of A_ps_array:", type(A_ps_array))
-# print("Type of f_ps_array:", type(f_ps_array))
-# print("Type of d_ps_array:", type(d_ps_array))
-
-plot_results(sol, A_ps, f_ps, d_ps)
+max_temp_value, instability_bool = optimize_pressure(best_params['A_ps'], best_params['f_ps'], plot=True, verbose=True)
